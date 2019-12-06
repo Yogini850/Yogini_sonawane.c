f768960472bfd5472c0d2658c83c9b23821095f3
Yogini850 <58591901+Yogini850@users.noreply.github.com>
2019-12-06 02:40:20 -0800

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-    int n, i, flag = 0;
+    int n,flag = 0;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-    for (i = 2; i < n / 2; ++i) 
+    for (int i = 2; i < n / 2; ++i) 
         if (n % i == 0) {
             flag = 1;
             break;
